18ad8bdbb6f00d55a6150779f46ab1b281284e19
ashirogimuta <quizandpuzzle@gmail.com>
2023-07-19 20:26:42 +0300

    Setting up scene

--- Assets/Scripts/TerrainComponent.cs
+++ Assets/Scripts/TerrainComponent.cs
@@ -8,7 +8,7 @@ namespace DynamicLOD
     {
         [SerializeField] private MeshFilter _meshFilter;
         [SerializeField] private MeshRenderer _meshRenderer;
-        [SerializeField] private Transform _effector;
+        [SerializeField] private Transform[] _effectors;
         [SerializeField] private MeshWireframeComputor _wireframeComputor;
         [SerializeField] private int _lodLevel;
         [SerializeField] private int _size;
@@ -21,7 +21,12 @@ namespace DynamicLOD
         private void Update()
         {
             Terrain terrain = new Terrain(transform.position,_size, _lodLevel);
-            terrain.SetTerrainEffector(new Vector2(_effector.position.x, _effector.position.z));
+            
+            foreach (Transform effector in _effectors)
+            {
+                terrain.SetTerrainEffector(new Vector2(effector.position.x, effector.position.z));
+            }
+           
             TerrainMeshGenerator meshGenerator = new TerrainMeshGenerator(terrain);
             _meshFilter.mesh = meshGenerator.BuildMesh();
             _wireframeComputor.UpdateMesh();
